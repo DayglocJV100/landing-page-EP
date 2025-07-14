d4a91cb53534861b3165153a8972168ea0c56052
DayglocJV100 <joaovyctorlima06@gmail.com>
2025-07-14 17:22:18 -0300

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,33 @@
+<?php
+// CONFIGURAÇÕES
+$destino = "comercial@experienciasportugal.com.br"; // altere para seu e-mail do domínio
+$assunto = "Nova mensagem do site Experiências Portugal";
+
+// RECEBE DADOS DO FORMULÁRIO
+$nome = $_POST['nome'] ?? '';
+$email = $_POST['email'] ?? '';
+$telefone = $_POST['telefone'] ?? '';
+$data = $_POST['data'] ?? '';
+$mensagem = $_POST['mensagem'] ?? '';
+
+// MONTA MENSAGEM
+$corpo = "Nova mensagem recebida pelo formulário:\n\n";
+$corpo .= "Nome: $nome\n";
+$corpo .= "E-mail: $email\n";
+$corpo .= "Telefone: $telefone\n";
+$corpo .= "Data de interesse: $data\n";
+$corpo .= "Mensagem:\n$mensagem\n";
+
+// CABEÇALHO
+$headers = "From: $email\r\n";
+$headers .= "Reply-To: $email\r\n";
+
+// ENVIA
+if (mail($destino, $assunto, $corpo, $headers)) {
+    header("Location: obrigado.html");
+exit();
+
+} else {
+    echo "<script>alert('Erro ao enviar mensagem. Tente novamente.');window.history.back();</script>";
+}
+?>
